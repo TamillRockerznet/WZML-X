6a2d6408f55899b1dbca6d2496253a60ce658d28
BARATH <166829997+RolexDas@users.noreply.github.com>
2025-04-02 19:19:07 +0530

    Update wzml_rockers.py
    Signed-off-by: BARATH <166829997+RolexDas@users.noreply.github.com>

--- bot/helper/themes/wzml_rockers.py
+++ bot/helper/themes/wzml_rockers.py
@@ -6,7 +6,7 @@ class WZMLStyle:
     ST_BN1_URL = "https://www.github.com/TamillRockerznet/WZML-X"
     ST_BN2_NAME = "Updates"
     ST_BN2_URL = "https://t.me/TamillRockerznet"
-    ST_MSG = """<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
+    ST_MSG = """<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>\n\n
 <b>Type {help_command} to get a list of available commands</b>"""
     ST_BOTPM = """<i>Now, This bot will send all your files and links here. Start Using ...</i>"""
     ST_UNAUTH = """<i>You Are not authorized user!</i>"""
